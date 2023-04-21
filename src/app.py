ec0c2c40e4c68a6570d9d8c942fa27c93f390c2b
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2023-04-21 16:57:05 -0600

    editar paciente

--- src/app.py
+++ src/app.py
@@ -255,6 +255,50 @@ def crearexpediente2():
     except Exception as ex:
         return render_template('crearexpediente.html')
 
+@app.route('/editarexpediente')
+@login_required
+def editarexpediente():
+    return render_template('editarexpediente.html')
+
+#EDICION
+
+@app.route('/editarexpediente2', methods=['POST'])
+@login_required
+def editarexpediente2():
+    try:
+        id_paciente = request.form['id_paciente']
+        dpi = request.form['dpi']
+        nombre = request.form['nombre']
+        telefono = request.form['telefono']
+        direccion = request.form['direccion']
+        imc = request.form['imc']
+        altura = request.form['altura']
+        peso = request.form['peso']
+        adiccion = request.form['adiccion']
+        enfermedades_hereditarias = request.form['enfermedades_hereditarias']
+        tratamientos = request.form['tratamientos']
+        medico_asignado = request.form['medico_asignado']
+        hospital_asignado = request.form['hospital_asignado']
+        enfermedades = request.form['enfermedades']
+        evolucion_enfermedad = request.form['evolucion_enfermedad']
+        fecha_ingreso = request.form['fecha_ingreso']
+        fecha_salida = request.form['fecha_salida']
+        hora_atencion = request.form['hora_atencion']
+
+        with connection.cursor() as cursor:
+                cursor.execute("""UPDATE pacientes SET id_paciente = %s, dpi = %s, nombre = %s, telefono = %s, direccion = %s,
+                                imc = %s, altura = %s, peso = %s, adiccion = %s, enfermedades_hereditarias = %s, tratamientos = %s, 
+                                medico_asignado = %s, hospital_asignado = %s, enfermedades = %s, evolucion_enfermedad = %s, 
+                                fecha_ingreso = %s, fecha_salida = %s, hora_atencion = %s
+                                WHERE id_paciente = %s""", (id_paciente, dpi, nombre, telefono, direccion, imc, altura, peso, adiccion, enfermedades_hereditarias, tratamientos, medico_asignado, hospital_asignado, enfermedades, evolucion_enfermedad, fecha_ingreso, fecha_salida, hora_atencion, id_paciente))
+                connection.commit()
+        cursor.close()
+        return render_template('confirmaciones2.html',)
+
+    except Exception as ex:
+        return render_template('editarmedicamento.html')
+
+
 #MUESTRA LA BITACORA DE CAMBIOS
 @app.route('/bitacora')
 @login_required
